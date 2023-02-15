9cd4438b032ac40c7e1173edf159fcfd15a38e6e
unknown <indraneeldey7@gmail.com>
2023-02-15 16:54:54 +0530

    Quora clone

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Login from "./components/auth/Login";
+import Quora from "./components/Quora";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
-
-  return (
-    <div id="main">
-    </div>
-  )
+function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            email: authUser.email,
+            displayName: authUser.displayName,
+            photo: authUser.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+      console.log(authUser);
+    });
+  }, [dispatch]);
+  return <div className="App">{user ? <Quora /> : <Login />}</div>;
 }
-
 
 export default App;
